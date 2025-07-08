9095650c797095f78516fe44cb04b4c964bda027
leduc <leanhduc140905@gmail.com>
2025-07-08 10:10:22 +0700

    move layout out of folder component

--- frontend/src/components/layout/EmptyLayout.jsx
+++
@@ -1,8 +0,0 @@
-// EmptyLayout.jsx
-// Chỉ render con, không có NavBar
-import React from 'react';
-import { Outlet } from 'react-router-dom';
-
-const EmptyLayout = () => <Outlet />;
-
-export default EmptyLayout;
