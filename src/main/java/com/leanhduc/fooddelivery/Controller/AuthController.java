f724c82a7e5ffd1d124e4c6169f02a7e1085fbac
leduc <leanhduc140905@gmail.com>
2025-09-08 21:04:17 +0700

    Change due to moving the JWT util file to the util package

--- src/main/java/com/leanhduc/fooddelivery/Controller/AuthController.java
+++ src/main/java/com/leanhduc/fooddelivery/Controller/AuthController.java
@@ -16,8 +16,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
-import static com.leanhduc.fooddelivery.Configuration.CookieUtil.clearRefreshTokenCookie;
-import static com.leanhduc.fooddelivery.Configuration.CookieUtil.setRefreshTokenCookie;
+import static com.leanhduc.fooddelivery.Utils.CookieUtil.clearRefreshTokenCookie;
+import static com.leanhduc.fooddelivery.Utils.CookieUtil.setRefreshTokenCookie;
 
 @RestController
 @AllArgsConstructor
